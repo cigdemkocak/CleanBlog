1d68ad0d92e352e0697190018854f0808e84aa2b
Çiğdem Koçak <kocakcigdem9@gmail.com>
2021-10-08 16:47:54 +0300

    Veritabanı düzenlemeleri yapıldı

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const ejs = require('ejs');
+const Post = require('./models/Post');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost/cleanblog-test-db',{
+  useNewUrlParser : true,
+  useUnifiedTopology : true,
+})
 
 //TEMPLATE ENGINE
 app.set("view engine" , "ejs");
 
 // MIDDLEWARE
 app.use(express.static('public'));
+app.use(express.urlencoded({extended : true}));
+app.use(express.json())
 
 //ROUTES
-app.get('/', (req, res) => {
-  
-  res.render('index')
+app.get('/', async (req, res) => {
+  const posts = await Post.find({});
+  res.render('index',{
+    posts
+  });
 });
 app.get('/about', (req, res) => {
   
@@ -23,7 +34,10 @@ app.get('/add_post', (req, res) => {
   
   res.render('add_post')
 });
-
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body)
+  res.redirect('/')
+});
 
 const port = 3000;
 app.listen(port, () => {
