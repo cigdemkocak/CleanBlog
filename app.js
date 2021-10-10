548947763f71943bb8ea5b96b91c20715fc6f12c
Çiğdem Koçak <kocakcigdem9@gmail.com>
2021-10-10 17:10:46 +0300

    MVC düzenlemesi yapıldı

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
+const methodOverride = require('method-override')
 const path = require('path');
 const ejs = require('ejs');
+
 const Post = require('./models/Post');
+const postControllers = require('./controllers/postControllers')
+const pageController = require('./controllers/pageController');
 
 const app = express();
 
@@ -17,33 +22,23 @@ app.set("view engine" , "ejs");
 // MIDDLEWARE
 app.use(express.static('public'));
 app.use(express.urlencoded({extended : true}));
-app.use(express.json())
+app.use(express.json());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
 //ROUTES
-app.get('/', async (req, res) => {
-  const posts = await Post.find({});
-  res.render('index',{
-    posts
-  });
-});
-app.get('/post/:id', async (req, res) => {
-  const post = await Post.findById(req.params.id)
-  res.render('post', {
-    post
-  })
-});
-app.get('/about', (req, res) => {
-  
-  res.render('about')
-});
-app.get('/add_post', (req, res) => {
-  
-  res.render('add_post')
-});
-app.post('/posts', async (req, res) => {
-  await Post.create(req.body)
-  res.redirect('/')
-});
+app.get('/', postControllers.getAllPosts);
+app.get('/posts/:id', postControllers.getPost);
+app.post('/posts', postControllers.createPost);
+app.put('/posts/:id', postControllers.updatePost);
+app.delete('/posts/:id', postControllers.deletePost);
+
+app.get('/about', pageController.getAboutPage);
+app.get('/add_post', pageController.getAddPostPage);
+app.get('/posts/edit/:id', pageController.getEditPage);
 
 const port = 3000;
 app.listen(port, () => {
